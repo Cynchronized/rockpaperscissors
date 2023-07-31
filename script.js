d392f03fa2e297f21bf328da832cb3d15c928b32
Cynchronized <austin.thieu123@gmail.com>
2023-07-30 22:17:56 -0600

    Finished

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+const selectionButtons = document.querySelectorAll('[data-selection]')
+const computerScoreSpan = document.querySelector('[data-computer-score]')
+const playerScoreSpan = document.querySelector('[data-player-score]')
+
 function getComputerChoice () {
     const computerChoice = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
-    let computerPlay = "rock";
+    let computerPlay = "rock"
 
     if (computerChoice == 1) {
-        computerPlay = "rock";
+        computerPlay = "rock"
     } else if (computerChoice == 2) {
         computerPlay = "scissors"
     } else {
@@ -13,43 +17,55 @@ function getComputerChoice () {
 }
 
 function playRound (playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
-    const playerWin = "You win! " + playerSelection + " beats " + computerSelection;
-    const playerLose = "You lose! " + playerSelection + " loses to " + computerSelection;
-    const tie = "You tie! " + playerSelection + " ties with " + computerSelection;
-    let victory = 1;
+    playerSelection = playerSelection.toLowerCase()
+    computerSelection = computerSelection.toLowerCase()
+    const playerWin = "You win! " + playerSelection + " beats " + computerSelection
+    const playerLose = "You lose! " + playerSelection + " loses to " + computerSelection
+    const tie = "You tie! " + playerSelection + " ties with " + computerSelection
+    let outcome = 1;
 
     if (playerSelection == "rock" && computerSelection == "scissors" || playerSelection == "scissors" && computerSelection == "paper" || 
         playerSelection == "paper" && computerSelection == "rock") {
-        console.log(playerWin);
-        return victory = 1;
+        console.log(playerWin)
+        return outcome = 1
     } else if (playerSelection == computerSelection) {
-        console.log(tie);
-        return victory = 2;
+        console.log(tie)
+        return outcome = 2
     } else {
-        console.log(playerLose);
-        return victory = 3;
+        console.log(playerLose)
+        return outcome = 3
     }
 }
 
+function updateScore (scoreSpan) {
+    scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1
+}
 
-function game() {
-    let compScore = 0;
-    let playScore = 0;
-    let round;
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt("Rock, Paper or Scissors");
-        const computerSelection = getComputerChoice();
-        round = playRound(playerSelection, computerSelection);
+function resetScore (computerScoreSpan, playerScoreSpan) {
+    alert ("Game over! Try again")
+    computerScoreSpan.innerText = 0
+    playerScoreSpan.innerText = 0
+}
 
-        if (round == 1) {
-            playScore++;
-        }else if (round == 3) {
-            compScore++;
+
+selectionButtons.forEach(selectionButton => {
+    selectionButton.addEventListener('click', e => {
+        const selectionName = selectionButton.dataset.selection
+        const score = playRound(selectionName, getComputerChoice())
+
+        if (score === 1) {
+            updateScore (playerScoreSpan)
+            alert("You Win!")
+        }else if (score === 3) {
+            updateScore (computerScoreSpan)
+            alert("You lost!")
         }
-        console.log("Player Score: " + playScore + " Computer Score: " + compScore);
-    }
-}
 
-game();
+        if (parseInt(computerScoreSpan.innerText) === 5|| parseInt(playerScoreSpan.innerText) === 5) {
+            resetScore(computerScoreSpan, playerScoreSpan)
+        }
+
+
+    })
+})
+
